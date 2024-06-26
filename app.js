96333eb4a12893b972d880a74c044a9bfa009c43
Martín Birchenz <birchenzmartin@gmail.com>
2024-06-26 12:28:54 -0300

    En este commit, hice algunas rutas con express conectandolas con html

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-let fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
 
-console.log(fs);
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
+
+app.get("/", (req, res) => {
+  let htmlpath = path.resolve(__dirname, "./index.html");
+  res.sendFile(htmlpath);
+});
+
+app.get("/login", function (req, res) {
+  res.send("En este apartado, vamos a poder logear al usuario");
+});
